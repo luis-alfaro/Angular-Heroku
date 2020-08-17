f736ecfb3317de4d769f0b482c9edc53e6edbff6
Kike <lead123456@hotmail.com>
2020-08-17 17:25:15 -0500

    Ajuste de error en server.js (faltaba importar express).

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
 app.use(express.static('./dist/Angular-Heroku'));
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/Angular-Heroku/'});
 });
 app.listen(process.env.PORT || 8080);
+
+/*app.use(express.static(__dirname+'/dist/ng-blog'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+});*/
